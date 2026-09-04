6ad92adc3a3a35ef218964731cf5323bbbdeeee2
Goh Guan Le <2602026@sit.singaporetech.edu.sg>
2026-09-04 17:21:21 +0800

    Fifth Commit

--- hello.py
+++ hello.py
@@ -8,14 +8,17 @@ bio = "Fun Blogger"
 followers = 100
 
 username = input("Enter Username: ")
-age = input("Enter Age: ")
+age = int(input("Enter Age: "))
 category = input("Enter Content Category: ")
 
 print("\nInstagram Profile")
-print("=============")
+print("=================")
 print("Username: ", username)
 print("Bio: ", bio)
 print("Category: ", category)
+
+if age>40 and category == "fun":
+    print("You are old what is fun for you??")
 
 followers += 50
 print("Day 1: ", followers)
@@ -27,4 +30,3 @@ followers -= 10
 print("Day 3: ", followers)
 
 print("Followers: ", followers)
-
