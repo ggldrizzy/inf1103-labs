1ea83370ab1c23dbb60c05a569c18ecdb742e5c8
Goh Guan Le <2602026@sit.singaporetech.edu.sg>
2026-09-04 17:08:06 +0800

    Third Commit

--- hello.py
+++ hello.py
@@ -7,6 +7,16 @@ username = "cool blogger"
 bio = "Fun Blogger"
 followers = 100
 
+followers += 50
+print("Day 1: ", followers)
+
+followers += 20
+print("Day 2: ", followers)
+
+followers -= 10
+print("Day 3: ", followers)
+
 print("Username: ", username)
 print("Bio: ", bio)
 print("Followers: ", followers)
+
